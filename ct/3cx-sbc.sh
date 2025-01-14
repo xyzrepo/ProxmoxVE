dbb815231611af5019706f569c0159a961733b0c
Baker <masterde@users.noreply.github.com>
2025-01-15 00:24:21 +0300

    Create 3cx-sbc.sh

---
+++ ct/3cx-sbc.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2025
+# Author: XYZ (xyzrepo)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://www.3cx.com/docs/session-border-controller/
+
+# App Default Values
+APP="3CX SBC"
+var_tags="voip;sbc;3cx"
+var_cpu="1"
+var_ram="512"
+var_disk="10"
+var_os="debian"
+var_version="11"
+var_unprivileged="1"
+
+# App Output & Base Settings
+header_info "$APP"
+base_settings
+
+# Core
+variables
+color
+catch_errors
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} The installer will now configure the application.${CL}"
+
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/xyzrepo/ProxmoxVE/main/install/3cx-sbc-install.sh)"
