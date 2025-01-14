ca93caca3b4a4120c48fc5521f742fbeda0b13fc
Baker <masterde@users.noreply.github.com>
2025-01-15 00:49:40 +0300

    Update 3cx-sbc-install.sh

--- install/3cx-sbc-install.sh
+++ install/3cx-sbc-install.sh
@@ -9,48 +9,38 @@ source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
 color
 verb_ip6
 catch_errors
-setting_up_container
-network_check
-update_os
 
-msg_info "Installing Dependencies"
-$STD apt-get install -y \
-  curl \
-  gnupg \
-  wget
-msg_ok "Installed Dependencies"
+# Variables
+VMID=101  # Change to desired VM ID
+VM_NAME="3cx-debian"
+ISO_URL="https://downloads-global.3cx.com/downloads/debian12iso/debian-amd64-netinst-3cx.iso"
+ISO_FILE="debian-3cx.iso"
 
-msg_info "Installing 3CX SBC"
-wget -O- http://downloads.3cx.com/downloads/3cxsbc.tar | tar -xvf - &>/dev/null
-cd 3cxsbc
-read -rp "Enter the FQDN or IP of your 3CX Server: " SBC_SERVER_FQDN
-read -rp "Enter the Authentication Key for 3CX SBC: " SBC_AUTH_KEY
-./install.sh << EOF
-$SBC_SERVER_FQDN
-$SBC_AUTH_KEY
-EOF
-msg_ok "Installed 3CX SBC"
+msg_info "Downloading 3CX Debian ISO"
+wget -O $ISO_FILE $ISO_URL
+if [[ $? -ne 0 || ! -s $ISO_FILE ]]; then
+    msg_error "Failed to download the 3CX Debian ISO. Check the URL or network connection."
+    exit 1
+fi
+msg_ok "Downloaded 3CX Debian ISO"
 
-msg_info "Setting up Service"
-cat <<EOF >/etc/systemd/system/3cxsbc.service
-[Unit]
-Description=3CX Session Border Controller
-After=network-online.target
-[Service]
-Type=simple
-ExecStart=/usr/sbin/3cxsbc
-Restart=always
-RestartSec=5
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl enable --now 3cxsbc &>/dev/null
-msg_ok "Service Created and Started"
+msg_info "Creating Proxmox VM for 3CX SBC"
+qm create $VMID --name "$VM_NAME" --memory 1024 --cores 2 --net0 virtio,bridge=vmbr0
+msg_ok "Created VM $VMID ($VM_NAME)"
 
-motd_ssh
-customize
+msg_info "Importing ISO to VM"
+qm importdisk $VMID $ISO_FILE local-lvm
+qm set $VMID --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-$VMID-disk-0
+qm set $VMID --boot c --bootdisk scsi0
+msg_ok "ISO Attached to VM"
 
-msg_info "Cleaning up"
-$STD apt-get autoremove -y &>/dev/null
-$STD apt-get autoclean -y &>/dev/null
-msg_ok "Cleaned up"
+msg_info "Starting 3CX VM"
+qm start $VMID
+msg_ok "3CX VM Started"
+
+msg_info "Cleaning up ISO file"
+rm -f $ISO_FILE
+msg_ok "Cleaned up temporary files"
+
+msg_ok "Installation script completed successfully!\n"
+echo -e "${INFO} Access the Proxmox console for VM $VMID to complete the 3CX SBC installation."
